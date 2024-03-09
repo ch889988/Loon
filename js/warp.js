90c496432263450ec52052e086b8d5905d0b5bae
ch889988 <52271858+ch889988@users.noreply.github.com>
2024-03-09 14:56:36 +0800

    Update warp.js

--- js/warp.js
+++ js/warp.js
@@ -1,7 +1,7 @@
 const $ = new Env("☁ Cloudflare: 1️⃣ 1.1.1.1 v2.0.0(3).panel");
 const DataBase = {
 	"Panel": {
-		"Settings":{"Switch":true,"Title":"☁ 𝙒𝘼𝙍𝙋 𝙄𝙣𝙛𝙤","Icon":"lock.icloud.fill","IconColor":"#f48220","BackgroundColor":"#f6821f","Language":"auto"},
+		"Settings":{"Switch":true,"Title":"☁ 𝙒𝘼𝙍𝙋 𝙄𝙣𝙛𝙤","Icon":"","IconColor":"","BackgroundColor":"#f6821f","Language":"auto"},
 		"Configs": {
 			"Request":{"url":"https://api.cloudflareclient.com","headers":{"authorization":null,"content-Type":"application/json","user-Agent":"1.1.1.1/2109031904.1 CFNetwork/1327.0.4 Darwin/21.2.0","cf-client-version":"i-6.7-2109031904.1"}},
 			"i18n":{
